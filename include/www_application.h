808e6fbf7f4d98e9b0d2514892bdf2b044a6f77b
codewitch-honey-crisis <gaztoast@gmail.com>
2025-12-15 08:40:33 -0800

    refactor

--- include/www_application.h
+++ include/www_application.h
@@ -1,5 +1,4 @@
-#ifndef WWW_APPLICATION_H
-#define WWW_APPLICATION_H
+#pragma once
 #include <stddef.h>
 #include <stdint.h>
 #include <math.h>
@@ -9,6 +8,15 @@
 #ifdef NEOPIXEL
 #include "led_strip.h"
 #endif
+
+// used by the page handlers
+typedef struct {
+    char path_and_query[513];
+    int method;
+    void* handle;
+    int fd;
+} httpd_context_t;
+
 extern char enc_rfc3986[256];
 extern char enc_html5[256];
 #ifdef NEOPIXEL
@@ -17,46 +25,70 @@ extern led_strip_handle_t neopixel_handle;
 #if defined(SD_CS) || defined(SDMMC_D0)
 extern sdmmc_card_t* sd_card;
 #endif
+
+// not all compiler vendors implement stricmp
+int my_stricmp(const char* lhs, const char* rhs);
 /// @brief Sends data to a connected socket
 /// @param data The data to send
 /// @param len The length of the data
 /// @param arg The application defined context
-static void httpd_send_block(const char* data, size_t len, void* arg);
+void httpd_send_block(const char* data, size_t len, void* arg);
 /// @brief Send chunked data over a connected socket
 /// @param data The data to send
 /// @param len The length of the data
 /// @param arg The application defined context
-static void httpd_send_chunked(const char* data, size_t len, void* arg);
+void httpd_send_chunked(const char* data, size_t len, void* arg);
 // /// @brief Sends an expression to a connected socket
 // /// @param expr The expression
 // /// @param arg The application defined context
-// static void httpd_send_expr(int expr, void* arg);
+// void httpd_send_expr(int expr, void* arg);
 // /// @brief Sends an expression to a connected socket
 // /// @param expr The expression
 // /// @param arg The application defined context
-// static void httpd_send_expr(unsigned char expr, void* arg);
+// void httpd_send_expr(unsigned char expr, void* arg);
 /// @brief Sends an expression to a connected socket
 /// @param expr The expression
 /// @param arg The application defined context
-static void httpd_send_expr(float expr, void* arg);
+void httpd_send_expr(float expr, void* arg);
 /// @brief Sends an expression to a connected socket
 /// @param expr The expression
 /// @param arg The application defined context
-static void httpd_send_expr(time_t expr, void* arg);
+void httpd_send_expr(time_t expr, void* arg);
 /// @brief Sends an expression to a connected socket
 /// @param expr The expression
 /// @param arg The application defined context
-static void httpd_send_expr(const char* expr, void* arg);
+void httpd_send_expr(const char* expr, void* arg);
 /// @brief URL encodes a string
 /// @param enc The encoded output buffer
 /// @param size The size of the out buffer
 /// @param s The string to encode
 /// @param table The table to use (enc_rfc3986 or enc_html5)
 /// @return The encoded output buffer
-static char *httpd_url_encode(char *enc, size_t size, const char *s, const char *table);
+char *httpd_url_encode(char *enc, size_t size, const char *s, const char *table);
+/// @brief Decodes an URL encoded string
+/// @param dst The decoded output buffer
+/// @param dstlen The size of the output buffer
+/// @param src The source to encode
+/// @return The decoded output buffer
+char* httpd_url_decode(char* dst, size_t dstlen, const char* src);
+
+/// @brief Cracks a query string into name/value pairs
+/// @param next_query_part The next part of the string to crack
+/// @param out_name A buffer to hold the name
+/// @param name_size The size of the out buffer
+/// @param out_value A buffer to hold the value
+/// @param value_size The size of the out buffer
+/// @return The next query segment
+const char* httpd_crack_query(const char* next_query_part,
+                                     char* out_name, size_t name_size,
+                                     char* out_value, size_t value_size);
+/// @brief Retrieves the MIME content-type for the path
+/// @param path The path to check
+/// @return The MIME type
+const char* httpd_content_type(const char* path);
+
 typedef struct stat stat_t;
 /// @brief Gets the stat for a path
 /// @param path The path
 /// @return A stat_t structure with the file or directory info
-static stat_t fs_stat(const char* path);
-#endif
+stat_t fs_stat(const char* path);
---
+++ src/www_application.cpp
@@ -0,0 +1,277 @@
+#include <sys/stat.h>
+#include <sys/unistd.h>
+#include <string.h>
+#include "esp_http_server.h"
+#include "www_application.h"
+
+char enc_rfc3986[256] = {0};
+char enc_html5[256] = {0};
+
+typedef struct {
+    const char* ext;
+    const char* ctype;
+} httpd_content_entry;
+
+static httpd_content_entry httpd_content_types[] = {
+    {".aac", "audio/aac"},
+    {".avif", "image/avif"},
+    {".bin", "application/octet-stream"},
+    {".bmp", "image/bmp"},
+    {".css", "text/css"},
+    {".csv", "text/csv"},
+    {".doc", "application/msword"},
+    {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {".epub", "application/epub+zip"},
+    {".gz", "application/gzip"},
+    {".gif", "image/gif"},
+    {".ico", "image/x-icon"},
+    {".jar", "application/java-archive"},
+    {".js", "text/javascript"},
+    {".mjs", "text/javascript"},
+    {".json", "application/json"},
+    {".mid", "audio/midi"},
+    {".midi", "audio/midi"},
+    {".mp3", "audio/mpeg"},
+    {".mp4", "video/mp4"},
+    {".mpeg", "video/mpeg"},
+    {".ogg", "audio/ogg"},
+    {".otf", "font/otf"},
+    {".pdf", "application/pdf"},
+    {".ppt", "application/vnd.ms-powerpoint"},
+    {".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {".rar", "application/vnd.rar"},
+    {".rtf", "application/rtf"},
+    {".jpg", "image/jpeg"},
+    {".jpeg", "image/jpeg"},
+    {".png", "image/png"},
+    {".apng", "image/apng"},
+    {".htm", "text/html"},
+    {".html", "text/html"},
+    {".svg", "image/svg+xml"},
+    {".tar", "application/x-tar"},
+    {".tif", "image/tiff"},
+    {".tiff", "image/tiff"},
+    {".ttf", "font/ttf"},
+    {".txt", "text/plain"},
+    {".vsd", "application/vnd.visio"},
+    {".wav", "audio/wav"},
+    {".weba", "audio/webm"},
+    {".webm", "video/webm"},
+    {".webp", "image/webp"},
+    {".woff", "font/woff"},
+    {".woff2", "font/woff2"},
+    {".xhtm", "application/xhtml+xml"},
+    {".xhtml", "application/xhtml+xml"},
+    {".xls", "application/vnd.ms-excel"},
+    {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {".xml", "application/xml"},
+    {".zip", "application/zip"},
+    {".7z", "application/x-7z-compressed"}
+};
+static const size_t httpd_content_types_size = 54;
+const char* httpd_content_type(const char* path) {
+    const char* ext = strrchr(path,'.');
+    if(ext!=NULL) {
+        // could optimize with a binary search or DFA
+        for(size_t i = 0;i<httpd_content_types_size;++i) {
+            if(0==my_stricmp(httpd_content_types[i].ext,ext)) {
+                return httpd_content_types[i].ctype;
+            }
+        }
+    }
+    return "application/octet-stream";
+}
+char* httpd_url_encode(char* enc, size_t size, const char* s,
+                              const char* table) {
+    char* result = enc;
+    if (table == NULL) table = enc_rfc3986;
+    for (; *s; s++) {
+        if (table[(int)*s]) {
+            *enc++ = table[(int)*s];
+            --size;
+        } else {
+            snprintf(enc, size, "%%%02X", *s);
+            while (*++enc) {
+                --size;
+            }
+        }
+    }
+    if (size) {
+        *enc = '\0';
+    }
+    return result;
+}
+
+char* httpd_url_decode(char* dst, size_t dstlen, const char* src) {
+    char a, b;
+    while (*src && dstlen) {
+        if ((*src == '%') && ((a = src[1]) && (b = src[2])) &&
+            (isxdigit(a) && isxdigit(b))) {
+            if (a >= 'a') a -= 'a' - 'A';
+            if (a >= 'A')
+                a -= ('A' - 10);
+            else
+                a -= '0';
+            if (b >= 'a') b -= 'a' - 'A';
+            if (b >= 'A')
+                b -= ('A' - 10);
+            else
+                b -= '0';
+            *dst++ = 16 * a + b;
+            dstlen--;
+            src += 3;
+        } else if (*src == '+') {
+            *dst++ = ' ';
+            dstlen--;
+            src++;
+        } else {
+            *dst++ = *src++;
+            dstlen--;
+        }
+    }
+    if (dstlen) {
+        *dst++ = '\0';
+    }
+    return dst;
+}
+
+const char* httpd_crack_query(const char* next_query_part,
+                                     char* out_name, size_t name_size,
+                                     char* out_value, size_t value_size) {
+    if (!*next_query_part) return NULL;
+
+    const char start = *next_query_part;
+    if (start == '&' || start == '?') {
+        ++next_query_part;
+    }
+    size_t i = 0;
+    char* name_cur = out_name;
+    while (*next_query_part && *next_query_part != '=' &&
+           *next_query_part != '&' && *next_query_part != ';') {
+        if (i < name_size) {
+            *name_cur++ = *next_query_part;
+        }
+        ++next_query_part;
+        ++i;
+    }
+    if (name_size) {
+        *name_cur = '\0';
+    }
+    if (!*next_query_part || *next_query_part == '&' ||
+        *next_query_part == ';') {
+        if (value_size) {
+            *out_value = '\0';
+        }
+        return next_query_part;
+    }
+    ++next_query_part;
+    i = 0;
+    char* value_cur = out_value;
+    while (*next_query_part && *next_query_part != '&' &&
+           *next_query_part != ';') {
+        if (i < value_size) {
+            *value_cur++ = *next_query_part;
+        }
+        ++next_query_part;
+        ++i;
+    }
+    if (value_size) {
+        *value_cur = '\0';
+    }
+    return next_query_part;
+}
+
+void httpd_send_block(const char* data, size_t len, void* arg) {
+    if (!data || !len) {
+        return;
+    }
+    httpd_context_t* resp_arg = (httpd_context_t*)arg;
+    int fd = resp_arg->fd;
+    if (fd > -1) {
+        httpd_handle_t hd = (httpd_handle_t)resp_arg->handle;
+        httpd_socket_send(hd, fd, data, len, 0);
+    } else {
+        httpd_req_t* r = (httpd_req_t*)resp_arg->handle;
+        httpd_send(r, data, len);
+    }
+}
+
+void httpd_send_chunked(const char* buffer, size_t buffer_len,
+                               void* arg) {
+    char buf[64];
+    if (buffer && buffer_len) {
+        itoa(buffer_len, buf, 16);
+        strcat(buf, "\r\n");
+        httpd_send_block(buf,strlen(buf),arg);
+        httpd_send_block(buffer,buffer_len,arg);
+        httpd_send_block("\r\n",2,arg);
+        return;
+    }
+    httpd_send_block("0\r\n\r\n", 5, arg);
+}
+
+void httpd_send_expr(float expr, void* arg) {
+    if (isnan(expr)) {
+        return;
+    }
+    char buf[64] = {0};
+    sprintf(buf, "%0.2f", expr);
+    for (size_t i = sizeof(buf) - 1; i > 0; --i) {
+        char ch = buf[i];
+        if (ch == '0' || ch == '.') {
+            buf[i] = '\0';
+            if (ch == '.') {
+                break;
+            }
+        } else if (ch != '\0') {
+            break;
+        }
+    }
+    httpd_send_chunked(buf, strlen(buf), arg);
+}
+
+void httpd_send_expr(time_t time, void* arg) {
+    httpd_send_expr(ctime(&time), arg);
+}
+
+void httpd_send_expr(const char* expr, void* arg) {
+    if (!expr || !*expr) {
+        return;
+    }
+    httpd_send_chunked(expr, strlen(expr), arg);
+}
+
+// void httpd_send_expr(int expr, void* arg) {
+//     char buf[64];
+//     itoa(expr, buf, 10);
+//     httpd_send_chunked(buf, strlen(buf), arg);
+// }
+
+// void httpd_send_expr(unsigned char expr, void* arg) {
+//     char buf[64];
+//     sprintf(buf, "%02d", (int)expr);
+//     httpd_send_chunked(buf, strlen(buf), arg);
+// }
+
+stat_t fs_stat(const char* path) {
+    stat_t s;
+    stat(path, &s);
+    return s;
+}
+
+int my_stricmp(const char* lhs, const char* rhs) {
+    int result = 0;
+    while (!result && *lhs && *rhs) {
+        result = tolower(*lhs++) - tolower(*rhs++);
+    }
+    if (!result) {
+        if (*lhs) {
+            return 1;
+        } else if (*rhs) {
+            return -1;
+        }
+        return 0;
+    }
+    return result;
+}
+
